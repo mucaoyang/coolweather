e33509c0c0a5325f4424b2469e192a373a3adf0b
yangmeng <1320485815@qq.com>
2018-06-03 23:20:59 +0800

    完成遍历省市县三级列表的功能

---
+++ app/src/main/java/com/ym/coolweather/ChooseAreaFragment.java
@@ -0,0 +1,10 @@
+package com.ym.coolweather;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Administrator on 2018-06-03.
+ */
+
+public class ChooseAreaFragment extends Fragment {
+}
---
+++ app/src/main/java/com/ym/coolweather/util/Utility.java
@@ -0,0 +1,8 @@
+package com.ym.coolweather.util;
+
+/**
+ * Created by Administrator on 2018-06-03.
+ */
+
+public class Utility {
+}
