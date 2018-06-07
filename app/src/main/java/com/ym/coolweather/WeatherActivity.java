a9060ffcfb6d6d0e1de4236b1cd4d7a489439b0f
yangmeng <1320485815@qq.com>
2018-06-07 13:15:27 +0800

    添加后台自动更新天气的功能.

--- app/src/main/java/com/ym/coolweather/WeatherActivity.java
+++ app/src/main/java/com/ym/coolweather/WeatherActivity.java
@@ -1,5 +1,6 @@
 package com.ym.coolweather;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Build;
@@ -21,6 +22,7 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.ym.coolweather.gson.Forecast;
 import com.ym.coolweather.gson.Weather;
+import com.ym.coolweather.service.AutoUpdateService;
 import com.ym.coolweather.util.HttpUtil;
 import com.ym.coolweather.util.Utility;
 
@@ -51,7 +53,7 @@ public class WeatherActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if(Build.VERSION.SDK_INT >=21){
+        if (Build.VERSION.SDK_INT >= 21) {
             View decorView = getWindow().getDecorView();
             decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
             getWindow().setStatusBarColor(Color.TRANSPARENT);
@@ -180,6 +182,9 @@ public class WeatherActivity extends AppCompatActivity {
         carWashText.setText(carWash);
         sportText.setText(sport);
         weatherLayout.setVisibility(View.VISIBLE);
+
+        Intent intent = new Intent(this, AutoUpdateService.class);
+        startService(intent);
     }
 
     /**
@@ -197,7 +202,7 @@ public class WeatherActivity extends AppCompatActivity {
             public void onResponse(Call call, Response response) throws IOException {
                 final String bingPic = response.body().string();
                 SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(WeatherActivity.this).edit();
-                editor.putString("bing_pic",bingPic);
+                editor.putString("bing_pic", bingPic);
                 editor.apply();
                 runOnUiThread(new Runnable() {
                     @Override
